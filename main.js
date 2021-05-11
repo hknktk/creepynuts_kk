9fd09e5ffef546af00206e5b5bb252c7af40b97a
hknktk <75674444+hknktk@users.noreply.github.com>
2021-05-11 14:47:23 +0900

    commit
    更新した。

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const menuButton = document.querySelector('.js-menu__button');
+const menu = document.querySelector('.js-menu');
 
+menuButton.addEventListener('click', () => {
+  menuopen();
+})
+
+function menuopen(){
+  menu.classList.toggle('show');
+}
