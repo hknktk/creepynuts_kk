a98c47a511627dcf64607d9ca648ff9990b23c82
hknktk <75674444+hknktk@users.noreply.github.com>
2021-05-14 15:54:48 +0900

    commit
    更新した。

---
+++ Creepy Nuts/main.js
@@ -0,0 +1,51 @@
+const menuButton = document.querySelector('.js-menu__button');
+const menu = document.querySelector('.js-menu');
+const menuList = document.querySelector('.js-menu__list');
+
+
+menuButton.addEventListener('click' , () => {
+  menuOpenClose();
+})
+
+menuList.addEventListener('click' , () => {
+  menuOpenClose();
+})
+
+
+function menuOpenClose(){
+  menu.classList.toggle('show');
+  menuButton.classList.toggle('active');
+}
+
+
+// テキストアニメーション
+const cb = function (el, isIntersecting) {
+  if(isIntersecting) {
+      const ta = new TextAnimation(el);
+      ta.animate();
+  }
+}
+
+const so = new ScrollObserver('.box__title', cb);
+
+
+const callback = function (el, isIntersecting) {
+  if(isIntersecting) {
+      el.classList.add('inview');
+  }
+} 
+
+
+const so2 = new ScrollObserver('.person__img', callback);
+
+
+
+
+ 
+
+
+
+
+
+
+
